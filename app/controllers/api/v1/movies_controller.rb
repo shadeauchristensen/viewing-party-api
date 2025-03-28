99a2a44923616b3fa35de5e5fcf8ab73f9075410
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-28 14:39:23 -0700

    Add sad path tests for top-rated movies and 100% coverage

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::MoviesController < ApplicationController
     def index
-        movies = MovieDbService.top_rated_movies[:results].first(20)
-
-        render json: MovieSerializer.format_movies(movies)
+        begin
+            data = MovieDbService.top_rated_movies[:results]
+            if data.nil?
+                render json: { error: "Movie data invalid, cannot return nil" }, status: :bad_request
+            else
+                movies = data.first(20)
+                render json: MovieSerializer.format_movies(movies)
+            end
+        rescue => error
+                render json: { error: "Unexpected error occurred: #{error.message}" }, status: :internal_server_error
+        end
     end
 end
---
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe "Top rated movies API", type: :request do
+    describe "GET api/v1/movies" do
+        it "HAPPY PATH: returns the top 20 highest rated movies", :vcr do
+            get "/api/v1/movies"
+
+            expect(response).to be_successful
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json).to be_an(Array)
+            expect(json.count).to eq(20)
+
+            json.each do | movie |
+                expect(movie).to have_key(:id)
+                expect(movie[:type]).to eq("movie")
+                expect(movie[:attributes]).to have_key(:title)
+                expect(movie[:attributes]).to have_key(:vote_average)
+            end
+        end
+
+        it "SAD PATH: returns errors if failed to retrieve the top 20" do
+            allow(MovieDbService).to receive(:top_rated_movies).and_return({ results: nil })
+
+            get "/api/v1/movies"
+
+            expect(response).not_to be_successful
+            expect(response).to have_http_status(:bad_request)
+
+            json = JSON.parse(response.body, symbolize_names: true)
+            
+            
+            expect(json).to have_key(:error)
+            expect(json[:error]).to eq("Movie data invalid, cannot return nil")            
+        end
+
+        it "SAD PATH: returns 500 error if unexpected error occurs" do
+            allow(MovieDbService).to receive(:top_rated_movies).and_raise(StandardError.new("Something went wrong"))
+
+            get "/api/v1/movies"
+
+            expect(response).to have_http_status(:internal_server_error)
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json).to have_key(:error)
+            expect(json[:error]).to eq("Unexpected error occurred: Something went wrong")
+        end
+    end
+end
