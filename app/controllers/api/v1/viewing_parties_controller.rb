afa12d49a6a9d8b50113f37fef88f7ead1e4cb71
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-29 14:20:23 -0700

    Fixed typo invitee[:id]

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -17,9 +17,9 @@ class Api::V1::ViewingPartiesController < ApplicationController
 
             params[:invitees].each do |invitee_id|
                 ViewingPartyUser.create!(
-                    user_id: invitee[:id],
-                    viewing_party_id: party.id,
-                    host: false
+                  user_id: invitee_id,
+                  viewing_party_id: party.id,
+                  host: false
                 )
             end
 
