825e7decbe8d0a0fc5dd9c883e02394fc649ac8e
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-30 12:18:19 -0700

    Fix missing comma in ViewingPartyUser create

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -11,11 +11,11 @@ class Api::V1::ViewingPartiesController < ApplicationController
                 movie_title: data[:movie_title]
             )
 
-            valid_invitees = data[:invitees].filter_map { |id| User.find_by =(id: id) }
+            valid_invitees = data[:invitees].filter_map { |id| User.find_by (id: id) }
 
             valid_invitees.each do |user|
                 ViewingPartyUser.create!(
-                    user_id: user.id
+                    user_id: user.id,
                     viewing_party_id: party.id
                 )
             end
