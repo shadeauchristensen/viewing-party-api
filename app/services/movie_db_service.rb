b518615848ca05b1bd511e306c8eed7677dec433
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-30 14:33:46 -0700

    MovieDB Fixes

--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -1,23 +1,22 @@
 class MovieDbService
-    def self.conn
-      Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
-        faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.movie_db[:key]}"
-      end
+  def self.conn
+    Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
+      faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.movie_db[:key]}"
     end
-  
-    def self.top_rated_movies
-      response = conn.get("movie/top_rated")
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  end
 
-    def self.search_movies(query)
-        response = conn.get("search/movie", { query: query })
-        JSON.parse(response.body, symbolize_names: true)
-    end
+  def self.top_rated_movies
+    response = conn.get("movie/top_rated")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    def self.movie_details(movie_id)
-      response = conn.get("movie/#{movie_id}")
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  def self.search_movies(query)
+    response = conn.get("search/movie", { query: query })
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_details(movie_id)
+    response = conn.get("movie/#{movie_id}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
