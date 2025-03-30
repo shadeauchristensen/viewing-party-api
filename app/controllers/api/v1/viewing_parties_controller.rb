77f12e0e56680d54c3f814341aa2a8814d508e0f
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-30 12:12:53 -0700

    ViewingPartiesController#create

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,53 +1,26 @@
 class Api::V1::ViewingPartiesController < ApplicationController
     def create
         begin
+            data = params.require(:data).require(:attributes)
 
-        puts params.inspect
-        attributes = party_params
-        host_id = party_params[:id].to_i
+            party = ViewingParty.create!(
+                name: data[:name],
+                start_time: data[:start_time],
+                end_time: data[:end_time],
+                movie_id: data[:movie_id],
+                movie_title: data[:movie_title]
+            )
 
-        party = ViewingParty.create!(
-          name: attributes[:name],
-          start_time: attributes[:start_time],
-          end_time: attributes[:end_time],
-          movie_id: attributes[:movie_id],
-          movie_title: attributes[:movie_title]
-        )
+            valid_invitees = data[:invitees].filter_map { |id| User.find_by =(id: id) }
 
-        ViewingPartyUser.create!(
-          user_id: host_id,
-          viewing_party_id: party.id,
-          host: true
-        )
+            valid_invitees.each do |user|
+                ViewingPartyUser.create!(
+                    user_id: user.id
+                    viewing_party_id: party.id
+                )
+            end
 
-        attributes[:invitees].each do |invitee_id|
-          ViewingPartyUser.create!(
-            user_id: invitee_id.to_i,
-            viewing_party_id: party.id,
-            host: false
-          )
+            render json: ViewingPartySerializer.new(party), status: :created
         end
-
-        render json: ViewingPartySerializer.new(party), status: :created 
-        rescue => error
-            # Rails.logger.error "CREATE PARTY ERROR: #{error.message}"
-            # Rails.logger.error error.backtrace.join("\n")
-            render json: { error: "Unexpected error occurred: #{error.message}" }, status: :internal_server_error
-        end
-    end
-
-    private
-
-    def party_params
-        params.require(:data).require(:attributes).permit(
-            :id,
-            :type,
-            :name,
-            :start_time,
-            :end_time,
-            :movie_id,
-            :movie_title,
-            invitees: []
-        )
     end
 end
