dc3a0cece6dd91a0cc80a72c976184c31434011a
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-29 17:39:15 -0700

    attempting corrections for postman

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,32 +1,34 @@
 class Api::V1::ViewingPartiesController < ApplicationController
     def create
-        begin
-            party = ViewingParty.create!(
-                name: params[:name],
-                start_time: params[:start_time],
-                end_time: params[:end_time],
-                movie_id: params[:movie_id],
-                movie_title: params[:movie_title]
-            )
-            # if viewing party is valid/ has correct user id, use serializer to create viewing party
-            ViewingPartyUser.create!(
-                user_id: params[:host_id],
-                viewing_party_id: party.id,
-                host: true
-            )
+      begin
+        attributes = params[:data][:attributes]
+        host_id = params[:data][:id].to_i
 
-            params[:invitees].each do |invitee_id|
-                ViewingPartyUser.create!(
-                  user_id: invitee_id.to_i,
-                  viewing_party_id: party.id,
-                  host: false
-                )
-            end
+        party = ViewingParty.create!(
+          name: attributes[:name],
+          start_time: attributes[:start_time],
+          end_time: attributes[:end_time],
+          movie_id: attributes[:movie_id],
+          movie_title: attributes[:movie_title]
+        )
 
-            render json: ViewingPartySerializer.new(party), status: :created 
-        rescue => error
-            # puts " ERROR TRACEBACK: #{error.full_message}"
-            render json: { error: "Unexpected error occurred: #{error.full_message}" }, status: :internal_server_error
+        ViewingPartyUser.create!(
+          user_id: host_id,
+          viewing_party_id: party.id,
+          host: true
+        )
+
+        attributes[:invitees].each do |invitee_id|
+          ViewingPartyUser.create!(
+            user_id: invitee_id.to_i,
+            viewing_party_id: party.id,
+            host: false
+          )
         end
+
+        render json: ViewingPartySerializer.new(party), status: :created 
+      rescue => error
+        render json: { error: "Unexpected error occurred: #{error.message}" }, status: :internal_server_error
+      end
     end
 end
