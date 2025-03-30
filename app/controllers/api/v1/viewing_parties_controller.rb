715f775edd7456d9921460c3bf9ecd26477ba433
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-30 13:09:58 -0700

    Fix missing_field reference in viewing parties controller

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -2,32 +2,31 @@ class Api::V1::ViewingPartiesController < ApplicationController
     def create
         begin
             data = params.require(:data).require(:attributes)
-
-            party = ViewingParty.create!(
-                name: data[:name],
-                start_time: data[:start_time],
-                end_time: data[:end_time],
-                movie_id: data[:movie_id],
-                movie_title: data[:movie_title]
-            )
-
-            valid_invitees = data[:invitees].filter_map { |id| User.find_by(id: id) }
-
-            valid_invitees.each do |user|
-                ViewingPartyUser.create!(
-                    user_id: user.id,
-                    viewing_party_id: party.id
-                )
-            end
-
-            render json: ViewingPartySerializer.new(party), status: :created
-
             required_fields = [:name, :start_time, :end_time, :movie_id, :movie_title]
 
-            required_fields.each do |field|
-                if data[field].blank?
-                    render json: { message: "Attribute #{field} is missing. Cannot be blank.", status: 400 }, status: :bad_request and return
+            missing_field = required_fields.find { |field| data[field].blank? }
+
+            if missing_field
+                render json: { message: "Attribute #{missing_field} is missing. Cannot be blank.", status: 400 }, status: :bad_request
+            else
+                party = ViewingParty.create!(
+                    name: data[:name],
+                    start_time: data[:start_time],
+                    end_time: data[:end_time],
+                    movie_id: data[:movie_id],
+                    movie_title: data[:movie_title]
+                )
+
+                valid_invitees = data[:invitees].filter_map { |id| User.find_by(id: id) }
+
+                valid_invitees.each do |user|
+                    ViewingPartyUser.create!(
+                        user_id: user.id,
+                        viewing_party_id: party.id
+                    )
                 end
+
+                render json: ViewingPartySerializer.new(party), status: :created
             end
         end
     end
