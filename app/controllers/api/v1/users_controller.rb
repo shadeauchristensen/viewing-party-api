9cfd0e4bcb42245877690ac94cbc710044e2fbfc
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-31 15:53:28 -0700

    Added endpoint for retirving the user profile. testing in postman

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,6 +12,14 @@ class Api::V1::UsersController < ApplicationController
     render json: UserSerializer.format_user_list(User.all)
   end
 
+  def show
+    begin
+      user = User.find(params[:id])
+      render json: UserSerializer.new(user)
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "Invalid User ID", status: 404 }, status: :bad_request
+  end
+
   private
 
   def user_params
