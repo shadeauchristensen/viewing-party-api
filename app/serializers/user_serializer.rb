a5847f7a43d0913188bd0ab2757f27440d6453ce
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-31 15:27:50 -0700

    Added serializer for the host_id moving onto controller now

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,36 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :username, :api_key
 
+  attribute :viewing_parties_hosted do |user|
+    user.viewing_parties_users.select(&:host).map do |vpu|
+      party = vpu.viewing_party
+      {
+        id: party.id,
+        name: party.name,
+        start_time: party.start_time,
+        end_time: party.end_time,
+        movie_id: party.movie_id,
+        movie_title: party.movie_title,
+        host_id: user.id,
+      }
+    end
+  end
+
+  attribute :viewing_parties_invited do |user|
+    user.viewing_parties_users.reject(&:host).map do |vpu|
+      party = vpu.viewing_party
+      {
+        id: party.id,
+        name: party.name,
+        start_time: party.start_time,
+        end_time: party.end_time,
+        movie_id: party.movie_id,
+        movie_title: party.movie_title,
+        host_id: party.viewing_parties_users.find(&:host).user_id,
+      }
+    end
+  end
+
   def self.format_user_list(users)
     { data:
         users.map do |user|
