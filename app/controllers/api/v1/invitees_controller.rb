97cb1031b12a2c9fda953709664f8e16e09c4301
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-30 19:51:13 -0700

    Controller invitees created, testing in postman

---
+++ app/controllers/api/v1/invitees_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::InviteesController < ApplicationController
+    def create
+        begin
+            party = ViewingParty.find(params[:viewing_party_id])
+            user = User.find(params[:invitees_user_id])
+
+            ViewingPartyUser.create!(viewing_party: party, user: user)
+
+            render json: ViewingPartySerializer.new(party), status: :created
+
+        rescue  ActiveRecord::RecordNotFound => error 
+            render json: { error.message }, status: :bad_request
+        rescue ActiveRecord::RecordInvalid => error
+            render json: { error.message }, status: :bad_request
+        end        
+    end
+end
