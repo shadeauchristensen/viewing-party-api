e30eed669a7cd8437472bbb6a5be85c791744c97
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-03-31 16:51:21 -0700

    Fix section in UserSerializer to test in postman

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -27,7 +27,7 @@ class UserSerializer
         end_time: party.end_time,
         movie_id: party.movie_id,
         movie_title: party.movie_title,
-        host_id: party.viewing_party_users.includes(:user).find(&:host)&.user&.id
+        host_id: party.viewing_party_users.find_by(host: true)&.user&.id
       }
     end
   end
